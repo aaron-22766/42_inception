6c47098ee8bfb0e97360456157537728603d07df
Aaron Rabenstein <arabenst@student.42heilbronn.de>
2023-12-12 17:54:02 +0100

    fixed env mariadb

--- srcs/requirements/mariadb/tools/start_mariadb.sh
+++ srcs/requirements/mariadb/tools/start_mariadb.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+sed -i "s/\${DB_NAME}/$DB_NAME/g"  /tmp/mariadb.sql;
+sed -i "s/\${DB_USER}/$DB_USER/g"  /tmp/mariadb.sql;
+sed -i "s/\${DB_PW}/$DB_PW/g"  /tmp/mariadb.sql;
+sed -i "s/\${DB_ROOT_PW}/$DB_ROOT_PW/g"  /tmp/mariadb.sql
+
 rc-service mariadb start;
 mariadb -u root < /tmp/mariadb.sql;
 rc-service mariadb stop;
-mariadbd -u root;
+exec mariadbd -u root;
